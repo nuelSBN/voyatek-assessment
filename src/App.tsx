87103fe8fa56308952341f412b43c29e03aa6c05
nuelsbn <echinedu007@gmail.com>
2024-11-08 19:11:49 +0100

    disbled mobile view

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
 import Layout from "./layout";
+import useIsMobile from "./hooks/useIsMobile";
 
 function App() {
+  const isMobile = useIsMobile();
+
+  if (isMobile) {
+    return (
+      <div className="h-dvh w-full flex flex-col justify-center items-center">
+        <img
+          src="https://media.giphy.com/media/3o7TKt17557q777212/giphy.gif"
+          alt="Mobile view is not available"
+        />
+        <h1 className="text-center font-semibold text-sm px-5">
+          Mobile view is not available, switch to desctop for better experience
+        </h1>
+      </div>
+    );
+  }
+
   return <Layout />;
 }
 
